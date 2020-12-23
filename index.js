5dae19c1d92d5d41968e2b1d604faa0c4aaa43eb
AnnaPCH <73597469+AnnaPCH@users.noreply.github.com>
2020-12-23 15:09:40 +0200

    changed the conversion button to links, css and html corrections

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 
 function showWeather(response) {
-  let cityElement = document.querySelector("h2");
+  let cityElement = document.querySelector("#city");
  cityElement.innerHTML = response.data.name;
   let temp = document.querySelector("#temperature");
   temp.innerHTML = Math.round(response.data.main.temp);
-  let deg = document.querySelector("#degrees");
-  deg.innerHTML = "°C";
   let description = document.querySelector("#description");
   description.innerHTML = response.data.weather[0].description ;
   let hum = document.querySelector("#humidity");
@@ -14,6 +12,8 @@ function showWeather(response) {
   wind.innerHTML = response.data.wind.speed;
   let windDegrees = document.querySelector("#wSpDeg");
   windDegrees.innerHTML = "m/s";
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 function showCity(event) {
@@ -30,60 +30,50 @@ function showCity(event) {
     city.innerHTML = cityValue;
   }
   }
-  function checkConvButton() {
-    let degrees = document.querySelector("#degrees"); 
-  if (degrees.innerHTML === "°C") {
+  function conversionF ()
+  {  preventDefault();
+    let c = document.querySelector("#conversion-f");
    let city = document.querySelector("#city-input");
-  let cityElement = document.querySelector("h2");
+  let cityElement = document.querySelector("#city");
   cityElement.innerHTML = city.value;
   let url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&appid=6870760fab0d60a8a4c52bbd8751c3cc&units=imperial`;
   axios.get(url).then(showWeatherImp);
-  } else {
-
-     let city = document.querySelector("#city-input");
-  let cityValue = document.querySelector("h2");
-  cityValue.innerHTML = city.value;
+  }  
+function conversionC () {
+ preventDefault();
+    let c = document.querySelector("#conversion-c");
+   let city = document.querySelector("#city-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = city.value;
   let url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&appid=6870760fab0d60a8a4c52bbd8751c3cc&units=metric`;
   axios.get(url).then(showWeatherMetric);
   }
-  }
+  
   function  showWeatherMetric (response) {
     let temp = document.querySelector("#temperature");
   temp.innerHTML =Math.round(response.data.main.temp);
-  let deg = document.querySelector("#degrees");
-  deg.innerHTML = "°C";
   let hum = document.querySelector("#humidity");
   hum.innerHTML = response.data.main.humidity;
   let wind = document.querySelector("#windspeed");
   wind.innerHTML = response.data.wind.speed;
   let windDegrees = document.querySelector("#wSpDeg");
   windDegrees.innerHTML = "m/s";
-  let convButton = document.querySelector("#conversion-btn");
-  convButton.innerHTML = "°C to °F";
-  convButton.addEventListener("click", checkConvButton);
   }
 
 function showWeatherImp(response) {
   
   let temp = document.querySelector("#temperature");
   temp.innerHTML =Math.round(response.data.main.temp);
-  let deg = document.querySelector("#degrees");
-  deg.innerHTML = "°F";
   let hum = document.querySelector("#humidity");
   hum.innerHTML = response.data.main.humidity;
   let wind = document.querySelector("#windspeed");
   wind.innerHTML = response.data.wind.speed;
   let windDegrees = document.querySelector("#wSpDeg");
-  windDegrees.innerHTML = "mi/h";
-  let convButton = document.querySelector("#conversion-btn");
-  convButton.innerHTML = "°F to °C";
-  
+  windDegrees.innerHTML = "mi/h"; 
 }
 
-let appDate = document.querySelector("h6");
-
-let now = new Date();
-
+function formatDate (timestamp) {
+let now = new Date(timestamp);
 let days = [
   "Sunday",
   "Monday",
@@ -94,7 +84,6 @@ let days = [
   "Saturday",
   "Sunday"
 ];
-
 let months = [
   "January",
   "February",
@@ -109,31 +98,23 @@ let months = [
   "November",
   "December"
 ];
-
 let year = now.getFullYear();
-
-let hour = now.getHours();
-
+let hour =now.getHours();
 let minutes = now.getMinutes();
-
 if (minutes <= 9) {
   minutes = `0${minutes}`;
 }
-
+if (hour<= 9) {
+  hour = `0${hour}`;
+}
 let date = now.getDate();
-
 let month = months[now.getMonth()];
-
 let day = days[now.getDay()];
-
-appDate.innerHTML = `${day}, ${month} ${date} ${year} ${hour}:${minutes}`; 
+return `${day}, ${month} ${date} ${year} ${hour}:${minutes}`; 
+}
 
 let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=Heraklion&appid=6870760fab0d60a8a4c52bbd8751c3cc&units=metric" ;
 axios.get(apiUrl).then(showWeather) ;
-
-let convButton = document.querySelector("#conversion-btn");
-
-convButton.addEventListener("click", checkConvButton);
 
 let searchForm = document.querySelector("#search-city-form");
 
